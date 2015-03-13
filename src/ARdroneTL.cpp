86d6614c203e280baf73a438120f77a857530b5f
LinGoWei <weiliu32992016@qq.com>
2015-03-14 00:38:30 +0800

    3th edition
    前飞，悬停，等

--- src/ARdroneTL.cpp
+++ src/ARdroneTL.cpp
@@ -1,17 +1,18 @@
 // 项目名：ARDroneTL 
 // 作  者：偷懒飞行员团队
-// 时  间：2015.02.08 
+// 时  间：2015.03.13
 
 #include <stdio.h>  
 #include <winsock2.h>  
 #include <assert.h>
 #include <iostream>
+#include <fstream>
 // C++11头文件
 #include <mutex>
 #include <thread>
 // 附加库
 #include <gtk/gtk.h>
-
+#include <gdk/gdkkeysyms.h>
 //本地头文件
 #include "MemoryLibrary.h"
 #pragma comment(lib, "ws2_32.lib") 
@@ -26,11 +27,13 @@ const int  NAVDATA_PORT = 5554;
 const int  VIDEO_PORT   = 5555;
 const int  AT_PORT      = 5556;
 
-const int INTERVAL      = 100;
-const char* ARDrone_IP	= "192.168.1.1";
-const int C_OK			= 1;							
-const int C_ERRO		= 0;
+const int INTERVAL = 100;
+const char* ARDrone_IP = "192.168.1.1";
+const int C_OK = 1;
+const int C_ERRO = 0;
 
+
+fstream outfile;
 
 /*************************************************
 * 类型声明模块
@@ -79,10 +82,8 @@ union INT_FLOAT_BUFFER
 *
 *【控制操作】
 * 已完成：起飞、降落、前进、后退、向左飞、向右飞、调整速度
-* 待完成：原地左旋转，原地右旋转
 *【成员变量操作】
 * 已完成：获取当前序号、获取前序号、获取后序号、设置前序号
-* 待完成：无
 *【发送指令】
 * 发送基础指令、发送飞行控制指令
 *【初始化操作】
@@ -94,7 +95,7 @@ union INT_FLOAT_BUFFER
 * 导航数据、前一个指令
 *【私有成员变量】
 * 发送指令的套接字（一套）、飞行速度、飞行器名字、包序号、前包序号、互斥锁
-
+*
 * 原则：有所为，有所不为
 */
 class ARDrone
@@ -108,9 +109,14 @@ public:
 	// 飞行器操作
 	void takeoff();
 	void land();
+	void hover();
 
 	void goingUp();
 	void goingDown();
+	void goingForward();
+	void goingBack();
+	void goingLeft();
+	void goingRight();
 
 	void turnLeft();
 	void turnRight();
@@ -118,12 +124,12 @@ public:
 	void setSpeed(int);		// 设置飞行速度
 
 public:
-	int		getCurrentSeq(){return this->seq_;}			// get the current sequence
-	int		getLastSeq(){return this->lastSeq_;}		// get the last sequence
+	int		getCurrentSeq(){ return this->seq_; }			// get the current sequence
+	int		getLastSeq(){ return this->lastSeq_; }		// get the last sequence
 	int		getNextSeq();								// get the next sequence
 	void	setLastSeq(int);							// set the last sequence
 
-	int		send_at_cmd(const char*);					// send all command
+	int		send_at_cmd(char*);					// send all command
 	int		send_pcmd(int, float, float, float, float);	// send control command
 	void	parse(MemoryLibrary::Buffer&);				// 数据包分析
 
@@ -131,10 +137,10 @@ protected:
 	void	initializeCmd();				// initialize command
 	void	initializeSocketaddr();			// initialize sockaddr_in
 	int		floatToInt(float);				// 使用联合体实现float 转化int	
-	
+
 public:
 	NAV_DATA		navData;			// ardrone's navdata 
-	const char*		at_cmd_last;		// save the last command
+	char*			at_cmd_last;		// save the last command
 
 private:
 	SOCKET		socketat_;		// socket
@@ -153,7 +159,6 @@ private:
 ARDrone::ARDrone(char* name)
 {
 	this->name_ = name;
-
 	// 用WSAStartup 启动Ws2_32.lib
 	WORD socketVersion = MAKEWORD(2, 2);
 	WSADATA wsaData;
@@ -167,8 +172,10 @@ ARDrone::ARDrone(char* name)
 	speed_		= 0.1f;
 	seq_		= 1;
 	lastSeq_	= 1;
-	at_cmd_last = "";
-	cout << "ARDrone initialized" <<endl;
+	at_cmd_last = new char[1024];
+
+	cout << "ARDrone initialized" << endl;
+	outfile << "ARDrone initialized" << endl;
 }
 
 // Destructors
@@ -176,7 +183,7 @@ ARDrone::~ARDrone()
 {
 	delete[] at_cmd_last;
 	delete[] name_;
-	
+
 	WSACleanup();					// 释放Winsock库
 	closesocket(this->socketat_);	// 关闭SOCKET
 }
@@ -184,10 +191,11 @@ ARDrone::~ARDrone()
 // initialize sockaddr_in
 void ARDrone::initializeSocketaddr()
 {
-	Atsin_.sin_family		= AF_INET;
-	Atsin_.sin_port			= htons(AT_PORT);
-	Atsin_.sin_addr.s_addr	= inet_addr(ARDrone_IP);
-	std::cout << "IP:" << ARDrone_IP << "Port:" << AT_PORT << std::endl;
+	Atsin_.sin_family = AF_INET;
+	Atsin_.sin_port = htons(AT_PORT);
+	Atsin_.sin_addr.s_addr = inet_addr(ARDrone_IP);
+	cout << "IP:" << ARDrone_IP << "Port:" << AT_PORT << std::endl;
+	outfile << "IP:" << ARDrone_IP << "Port:" << AT_PORT << std::endl;
 }
 
 // initialize command（没有使用）
@@ -217,38 +225,78 @@ void ARDrone::initializeCmd()
 // 飞行控制函数实现部分
 void ARDrone::takeoff()
 {
-	assert(send_at_cmd("AT*REF=1,290718208"));
-	printf("takeoff\n");
+	char cmd[1024];
+	sprintf_s(cmd, "AT*REF=%d,290718208\r", getNextSeq());
+	assert(send_at_cmd(cmd));
+	printf("take off\n");
+	outfile << "Take off" << endl;
 }
 
 void ARDrone::land()
 {
-	assert(send_at_cmd("AT*REF=1,290717696"));
+	char cmd[100];
+	sprintf_s(cmd, "AT*REF=%d,290717696\r", getNextSeq());
+	assert(send_at_cmd(cmd));
 	printf("land\n");
+	outfile << "Land" << endl;
+}
+
+//有问题？？？？？？？？？？？？？？？？？？？？？？？？？
+void ARDrone::hover()
+{
+	assert(send_pcmd(0, 0, 0, 0, 0));
+	outfile << "Hover" << endl;
 }
 
 void ARDrone::goingUp()
 {
 	assert(send_pcmd(1, 0, 0, speed_, 0));
 	printf("goingUp\n");
+	outfile << "Going up" << endl;
 }
 
 void ARDrone::goingDown()
 {
 	assert(send_pcmd(1, 0, 0, -speed_, 0));
 	printf("goingDown\n");
+	outfile << "Going down" << endl;
+}
+
+void ARDrone::goingForward()
+{
+	assert(send_pcmd(1, 0, -speed_, 0, 0));
+	outfile << "Going Forward" << endl;
+}
+
+void ARDrone::goingBack()
+{
+	assert(send_pcmd(1, 0, +speed_, 0, 0));
+	outfile << "Going Back" << endl;
+}
+void ARDrone::goingLeft()
+{
+	assert(send_pcmd(1, -speed_, 0, 0, 0));
+	outfile << "Going Left" << endl;
+}
+
+void ARDrone::goingRight()
+{
+	assert(send_pcmd(1, speed_, 0, 0, 0));
+	outfile << "Going Right" << endl;
 }
 
 void ARDrone::turnLeft()
 {
 	assert(send_pcmd(1, 0, 0, 0, -speed_));
-	printf("turnLeft\n");
+	printf("turn Left\n");
+	outfile << "Turn left" << endl;
 }
 
 void ARDrone::turnRight()
 {
 	assert(send_pcmd(1, 0, 0, 0, speed_));
 	printf("turnRight\n");
+	outfile << "Turn right" << endl;
 }
 
 void ARDrone::setSpeed(int mul)
@@ -275,15 +323,16 @@ void ARDrone::setLastSeq(int currentSeq)
 // send control command
 int ARDrone::send_pcmd(int enable, float roll, float pitch, float gaz, float yaw)
 {
+	outfile<< "speed:" << this->speed_<<endl;
 	char cmd[1024];
-	sprintf_s(cmd, "AT*PCMD=%d,%d,%d,%d,%d,%d", getNextSeq(), enable,
-			floatToInt(roll), floatToInt(pitch), floatToInt(gaz), floatToInt(yaw));
+	sprintf_s(cmd, "AT*PCMD=%d,%d,%d,%d,%d,%d\r", getNextSeq(), enable,
+		floatToInt(roll), floatToInt(pitch), floatToInt(gaz), floatToInt(yaw));
 	int result = send_at_cmd(cmd);
 	return result;
 }
 
 // send all command
-int ARDrone::send_at_cmd(const char* cmd)
+int ARDrone::send_at_cmd(char* cmd)
 {
 	// 互斥锁:用于多线程C++11
 	this->mtx.lock();
@@ -293,6 +342,10 @@ int ARDrone::send_at_cmd(const char* cmd)
 		return C_ERRO;
 
 	printf_s("AT command: %s\n", cmd);
+
+	
+	outfile << "AT command: " << cmd << endl;
+
 	this->mtx.unlock();
 	return C_OK;
 }
@@ -313,48 +366,74 @@ void ARDrone::parse(MemoryLibrary::Buffer& buffer)
 	if (header != 0x55667788)
 	{
 		cout << "NavigationDataReceiver FAIL, because the header != 0x55667788\n";
+		outfile << "NavigationDataReceiver FAIL, because the header != 0x55667788" << endl;
 		return;
 	}
 	// /////////////////////////////////////////////////////////////////(Test)show in console
 	offset += 4;
 	int state = buffer.MakeValueFromOffset<int32_t>(offset);
-	cout << "state: "<< state<< endl;
+	cout << "state: " << state << endl;
+	outfile << "state: " << state << endl;
+
 	offset += 4;
 	int sequence = buffer.MakeValueFromOffset<int32_t>(offset);
-	cout << "sequence: "<< sequence<< endl;
+	cout << "sequence: " << sequence << endl;
+	outfile << "sequence: " << sequence << endl;
 
 	offset += 4;
 	int visionDefined = buffer.MakeValueFromOffset<int32_t>(offset);
-	cout << "visionDefined:"<< visionDefined<< endl;
+	cout << "visionDefined:" << visionDefined << endl;
+	outfile << "visionDefined:" << visionDefined << endl;
+
 	offset += 4;
 	int16_t tag = buffer.MakeValueFromOffset<int16_t>(offset);
 	cout << "tag: " << tag << endl;
+	outfile << "tag: " << tag << endl;
+
 	offset += 2;
 	int16_t size = buffer.MakeValueFromOffset<int16_t>(offset);
 	cout << "size: " << size << endl;
+	outfile << "size: " << size << endl;
+
 	offset += 2;
 	int ctrlState = buffer.MakeValueFromOffset<int32_t>(offset);
 	cout << "ctrlState: " << ctrlState << endl;
+	outfile << "ctrlState: " << ctrlState << endl;
 
 	offset += 4;
 	cout << "batteryLevel: " << buffer.MakeValueFromOffset<int>(offset) << "%" << endl;
+	outfile << "batteryLevel: " << buffer.MakeValueFromOffset<int>(offset) << "%" << endl;
+
 	offset += 4;
 	cout << "pitch: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+	outfile << "pitch: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+
 	offset += 4;
 	cout << "roll: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+	outfile << "roll: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+
 	offset += 4;
-	cout << "yaw" << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+	cout << "yaw: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+	outfile << "yaw: " << buffer.MakeValueFromOffset<float>(offset) / 1000.0f << endl;
+
 	offset += 4;
 	cout << "altitude: " << (float)buffer.MakeValueFromOffset<int>(offset) / 1000.0f << endl;
+	outfile << "altitude: " << (float)buffer.MakeValueFromOffset<int>(offset) / 1000.0f << endl;
 
 	offset += 4;
 	cout << "vx: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+	outfile << "vx: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+
 	offset += 4;
 	cout << "vy: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+	outfile << "vy: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+
 	offset += 4;
 	cout << "vz: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+	outfile << "vz: " << buffer.MakeValueFromOffset<float>(offset) << endl;
+
 	offset += 4;
-	
+
 	////////////////////////////////////////////////////////////////// set  
 	offset = 0;
 	this->navData.header = buffer.MakeValueFromOffset<int32_t>(offset);
@@ -365,14 +444,14 @@ void ARDrone::parse(MemoryLibrary::Buffer& buffer)
 	offset += 4;
 	this->navData.visionDefined = buffer.MakeValueFromOffset<int32_t>(offset);
 	offset += 4;
-	
+
 	this->navData.tag = buffer.MakeValueFromOffset<int16_t>(offset);
 	offset += 2;
 	this->navData.size = buffer.MakeValueFromOffset<int16_t>(offset);
 	offset += 2;
 	this->navData.ctrlState = buffer.MakeValueFromOffset<int32_t>(offset);
 	offset += 4;
-	
+
 	this->navData.batteryLevel = buffer.MakeValueFromOffset<int32_t>(offset);
 	offset += 4;
 	this->navData.altitude = buffer.MakeValueFromOffset<int32_t>(offset);
@@ -396,65 +475,75 @@ void ARDrone::parse(MemoryLibrary::Buffer& buffer)
 /********************************
 * 消息响应控制模块（control）
 *********************************/
+
+// 窗口销毁
+void destroy(GtkWidget *widget, gpointer data)
+{
+	gtk_main_quit();
+}
+
 // 键盘控制消息响应函数
 void control(GtkWidget* widget, GdkEventKey* event, gpointer data)
 {
 	ARDrone* ardrone = (ARDrone*)data;
-	switch (event->keyval)
+	guint keyValue = event->keyval;
+	switch (keyValue)
 	{
-		case GDK_KEY_KP_Enter:
-			ardrone->takeoff(); break;
-		case GDK_KEY_BackSpace:
-			ardrone->land(); break;
-		case GDK_KEY_W: 
-			ardrone->goingUp(); break;
-		case GDK_KEY_S: 
-			ardrone->goingDown(); break;
-		case GDK_KEY_A: 
-			ardrone->turnLeft(); break;
-		case GDK_KEY_D: 
-			ardrone->turnRight(); break;
-		default: 
-			printf("指令不支持\n");break;
+	case 1://1
+		ardrone->takeoff(); break;
+	case 50://2
+		ardrone->land(); break;
+	case 51://3
+		ardrone->goingForward(); break;
+	case 52://4
+		ardrone->hover(); break;
+	case 53://5
+		ardrone->turnLeft(); break;
+	case 54://6
+		ardrone->turnRight(); break;
+	default:
+		printf("指令不支持\n"); 
+		outfile << "指令不支持" << endl; break;
 	}
+}
+
+void keyReleaseEvent(GtkWidget* widget, GdkEventKey* event, gpointer data)
+{
+	ARDrone* ardrone = (ARDrone*)data;
+	guint keyValue = event->keyval;
+	outfile << keyValue << endl;
+	if (keyValue != 49 || keyValue != 50) 
+		return;
+	ardrone->hover();
 }
 
 /******************************************
 * UI 模块 (VIEW)
 ******************************************/
-
-// 窗口销毁
-void destroy (GtkWidget *widget, gpointer data)
-{
-    gtk_main_quit ();
-}
-
 // UI 初始化
-void initGtk(int argc, char* argv[])
+void initGtk(int argc, char* argv[], ARDrone* ardrone)
 {
 	gtk_init(&argc, &argv);
 
 	// 窗体属性设置
-	arui->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	arui->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(arui->window), "myARDrone");
-    gtk_window_set_default_size(GTK_WINDOW(arui->window), 400, 300);
-    gtk_window_set_position(GTK_WINDOW(arui->window), GTK_WIN_POS_CENTER);
-    gtk_container_set_border_width(GTK_CONTAINER(arui->window), 40);
-	
+	gtk_window_set_default_size(GTK_WINDOW(arui->window), 400, 300);
+	gtk_window_set_position(GTK_WINDOW(arui->window), GTK_WIN_POS_CENTER);
+	gtk_container_set_border_width(GTK_CONTAINER(arui->window), 40);
+	/*
 	// 控件设置
-	arui->box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);		// 容器控件在水平方位
+	arui->box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);		// 容器控件在水平方位
 	gtk_container_add(GTK_CONTAINER(arui->window), arui->box);
 	arui->buffer = gtk_text_buffer_new(NULL);
 	arui->view = gtk_text_view_new_with_buffer(arui->buffer);
 	gtk_box_pack_start(GTK_BOX(arui->box), arui->view, TRUE, TRUE, 10);
 	gtk_widget_set_size_request(arui->view, 10, 15);
-
+	*/
 	// 消息响应
-	g_signal_connect(G_OBJECT (arui->window), "destroy", G_CALLBACK(destroy), NULL);
-
-	// button = gtk_button_new_with_label("getNavData");
-	// g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(button_event), NULL);
-	// gtk_box_pack_start(GTK_BOX(box), button, TRUE, TRUE, 10);
+	g_signal_connect(G_OBJECT(arui->window), "destroy", G_CALLBACK(destroy), NULL);
+	g_signal_connect(G_OBJECT(arui->window), "key_press_event", G_CALLBACK(control), ardrone);
+	//g_signal_connect(G_OBJECT(arui->window), "key_release_event", G_CALLBACK(keyReleaseEvent), ardrone);
 
 	gtk_widget_show_all(arui->window);
 }
@@ -466,9 +555,9 @@ void initGtk(int argc, char* argv[])
 // 保持与Ardrone连接的线程
 void weakUpThread(ARDrone* ardrone)
 {
-	char cmd[1024]	= { 0 };
-	int delay		= 0;
-	while( true)
+	char cmd[1024] = { 0 };
+	int delay = 0;
+	while (true)
 	{
 		// keep weak up
 		Sleep(40);
@@ -489,33 +578,35 @@ void weakUpThread(ARDrone* ardrone)
 // 获取导航数据的线程
 void NavDataThread(ARDrone* ardrone)
 {
-	SOCKET socketNav_		= socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	SOCKET socketNav_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	sockaddr_in	navSin_;
-	navSin_.sin_family		= AF_INET;
-	navSin_.sin_port		= htons(NAVDATA_PORT);
+	navSin_.sin_family = AF_INET;
+	navSin_.sin_port = htons(NAVDATA_PORT);
 	navSin_.sin_addr.s_addr = inet_addr(ARDrone_IP);
-	int lenNavSin_			= sizeof(navSin_);
+	int lenNavSin_ = sizeof(navSin_);
 
 	// 激活指令发往NAVDATA_PORT 端口
 	const char trigger[4] = { 0x01, 0x00, 0x00, 0x00 };
 	int result = sendto(socketNav_, trigger, strlen(trigger), 0, (sockaddr *)&navSin_, lenNavSin_);
 	if (result != SOCKET_ERROR)
+	{
 		printf_s("Sent trigger flag to UDP port : %d \n", NAVDATA_PORT);
+		outfile << "Sent trigger flag to UDP port : " << NAVDATA_PORT << endl;
+	}
 
 	// 配置指令发往AT_PORT 端口
 	char initCmd[1024] = { 0 };
 	sprintf_s(initCmd, "AT*CONFIG=%d,\"general:navdata_demo\",\"TRUE\"\r", ardrone->getNextSeq());
 	assert(ardrone->send_at_cmd(initCmd));
-	
+
 	// 接收数据包
 	MemoryLibrary::Buffer navDataBuffer;	// 二进制数据缓冲区
-	char recv[1024] = {0};					// 数据包接收数组
-	int lenRecv		= 0;
-	int delay		= 0;
+	char recv[1024] = { 0 };				// 数据包接收数组
+	int lenRecv = 0;
+	int delay = 0;
 	// UI 部分
-	char text[100]	= {0};					// 导航数据显示字符串
-	GtkTextIter start,end;					// 缓冲区开始和结束位置
-
+	char text[100] = { 0 };					// 导航数据显示字符串
+	
 	while (true)
 	{
 		lenRecv = recvfrom(socketNav_, recv, 1024, 0, (struct sockaddr*)&navSin_, &lenNavSin_);
@@ -524,13 +615,16 @@ void NavDataThread(ARDrone* ardrone)
 		{
 			delay = 0;
 			printf("received %d bytes\n", lenRecv);
+			outfile << "received " << lenRecv << " bytes" << endl;
 			navDataBuffer.Set(recv, lenRecv);
-			ardrone->parse(navDataBuffer);	
+			ardrone->parse(navDataBuffer);
 
 			sprintf_s(text, "batreeyLevel: %d%%  altitude: %d\n"
-					, ardrone->navData.batteryLevel, ardrone->navData.altitude); 
-			gtk_text_buffer_get_bounds(GTK_TEXT_BUFFER(arui->buffer), &start, &end);//获得缓冲区开始和结束位置的Iter	
-			gtk_text_buffer_set_text(GTK_TEXT_BUFFER(arui->buffer), text, 30);		//插入文本到缓冲区
+				, ardrone->navData.batteryLevel, ardrone->navData.altitude);
+			//outfile <<text;
+			// gtk_text_buffer_get_bounds(GTK_TEXT_BUFFER(arui->buffer), &start, &end);    //获得缓冲区开始和结束位置的Iter	
+			// gtk_text_buffer_set_text(GTK_TEXT_BUFFER(arui->buffer), text, -1);		//插入文本到缓冲区
+
 		}
 	}
 }
@@ -540,8 +634,11 @@ void NavDataThread(ARDrone* ardrone)
 ******************************************/
 int main(int argc, char* argv[])
 {
+	outfile.open("data.txt");
+	if(!outfile.is_open())
+		return 0;
 	ARDrone* ardrone = new ARDrone("myardrone");
-	initGtk(argc, argv);
+	initGtk(argc, argv, ardrone);
 
 	// C++11 声明导航数据的线程
 	std::thread navThread(NavDataThread, ardrone);
@@ -551,10 +648,10 @@ int main(int argc, char* argv[])
 	gtk_main();
 
 	// 空循环：UI关闭后保持其他线程运行
-	while(true)
+	while (true)
 	{
 		;
 	}
+	outfile.close();
 	return 0;
 }
-
